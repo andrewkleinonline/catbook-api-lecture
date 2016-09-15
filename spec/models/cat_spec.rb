3074c98a9ec3763a4f3e7468187b5a27faaabc05
Andrew Klein <andrew.klein.j@gmail.com>
2016-09-15 11:01:11 -0400

    lay out planned tests for the cat model

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should be valid with a name and weight'
+  it 'should be invalid without a name'
+  it 'should be invalid without a weight'
+  it 'should not be fluffy be default'
+  it 'should know if it is fat'
+  it 'should be able to learn a new hobby'
 end
