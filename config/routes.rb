11245e13ae4e6f6fd7b4f0a3a5a8bbdea8beba21
Andrew Klein <andrew.klein.j@gmail.com>
2016-09-15 12:01:11 -0400

    build out test for json cats index response

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # catbook.com/api/v1/cats
   namespace :api do
-    module :v1 do
+    namespace :v1 do
       resources :cats
       resources :hobbies
     end
--- spec/api/v1/cat_requests_spec.rb
+++ spec/api/v1/cat_requests_spec.rb
@@ -1,3 +1,15 @@
-describe "Cats API" do
+require "rails_helper"
 
+describe "Cats API" do
+  describe "get /cats" do
+    it 'returns a JSON collection of all of the cats' do
+      Cat.create(name: 'Maru', weight: 12, temperament: 'curious')
+      Cat.create(name: 'Grumpy Cat', weight: 9, temperament: 'grumpy')
+
+      get '/cats'
+
+      expect(response).to be_success
+
+    end
+  end
 end
