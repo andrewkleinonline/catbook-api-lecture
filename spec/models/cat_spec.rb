3b36affbb3e44418635a2ff857f75340b9ecb328
Andrew Klein <andrew.klein.j@gmail.com>
2016-09-15 11:21:38 -0400

    add hobby learning test to cat spec

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -28,10 +28,16 @@ RSpec.describe Cat, type: :model do
   it 'should know if it is fat' do
     cat = Cat.new(name: 'Maru', weight: 12)
     skinny_cat = Cat.new(name: 'Skinny', weight: 9)
-    
+
     expect(cat.fat?).to be(true)
     expect(skinny_cat.fat?).to be(false)
   end
 
-  it 'should be able to learn a new hobby'
+  it 'should be able to learn a new hobby' do
+    cat = Cat.new(name: 'Maru', weight: 12)
+    hobby = Hobby.new(name: 'Hiding', description: 'Some cats like to hide in cardboard boxes or stick their heads in paper bags')
+
+    cat.learn_to(hobby)
+    expect(cat.hobbies).to include(hobby)
+  end
 end
