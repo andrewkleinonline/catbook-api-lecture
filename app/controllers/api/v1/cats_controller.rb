38dab2e06429f6fe91a025eae734250ca4255355
Andrew Klein <andrew.klein.j@gmail.com>
2016-09-15 14:18:43 -0400

    build out show, create, and update tests for cat requests

--- app/controllers/api/v1/cats_controller.rb
+++ app/controllers/api/v1/cats_controller.rb
@@ -6,6 +6,35 @@ module Api
         render json: Cat.all
       end
 
+      def show
+        render json: Cat.find(params[:id])
+      end
+
+      def create
+        cat = Cat.create(cat_params)
+        if cat.save
+          render json: cat
+        else
+          render json: cat.errors, status: 500
+        end
+      end
+
+      def update
+        cat = Cat.find(params[:id])
+        if cat.update(cat_params)
+          render json: cat
+        else
+          render json: cat.errors, status: 500
+        end
+
+      end
+
+      private
+
+      def cat_params
+        params.require(:cat).permit(:name, :breed, :temperament, :weight)
+      end
+
     end
   end
 end
--- spec/api/v1/cat_requests_spec.rb
+++ spec/api/v1/cat_requests_spec.rb
@@ -8,10 +8,79 @@ describe "Cats API" do
 
       get '/api/v1/cats'
       response_body = JSON.parse(response.body)
-      binding.pry
       expect(response).to be_success
       expect(response_body.length).to equal(2)
 
     end
   end
+
+  describe "get /cats/:id" do
+    it 'returns a JSON collection of a single cat' do
+      cat = Cat.create(name: 'Maru', weight: 12, temperament: 'curious')
+      Cat.create(name: 'Grumpy Cat', weight: 9, temperament: 'grumpy')
+
+      get '/api/v1/cats/1'
+      response_body = JSON.parse(response.body)
+      expect(response).to be_success
+      expect(response_body).to be_a(Hash)
+      expect(response_body["name"]).to eq(cat.name)
+
+    end
+  end
+
+  describe 'post /cats' do
+    context 'when valid' do
+      it 'creates a new cat' do
+        post '/api/v1/cats', {cat: {name: "Mimi", breed: "pixie bob", temperament: "wild", weight: 7}}
+        cat = Cat.first
+        expect(Cat.count).to eq(1)
+        expect(cat.name).to eq("Mimi")
+      end
+    end
+
+    context 'when invalid' do
+      it 'returns an error status and message' do
+        post '/api/v1/cats', {cat: {breed: "pixie bob", temperament: "wild", weight: 7}}
+        #binding.pry
+        response_body = JSON.parse(response.body)
+
+        expect(response.status).to eq(500)
+        expect(response_body).to eq({"name" => ["can't be blank"]})
+      end
+
+    end
+  end
+
+  describe 'patch the cats' do
+    context 'when valid' do
+      it 'patches a cat' do
+        Cat.create(name: 'Maru', weight: 12, temperament: 'curious')
+        Cat.create(name: 'Grumpy Cat', weight: 9, temperament: 'grumpy')
+
+        patch '/api/v1/cats/1', {cat: {name: 'Andru', temperament: 'introspective & snarky'}}
+        response_body = JSON.parse(response.body)
+
+        expect(response).to be_success
+        expect(response_body["name"]).to eq("Andru")
+        expect(response_body["temperament"]).to eq("introspective & snarky")
+
+
+      end
+    end
+
+    context 'when invalid' do
+      it 'returns an error status and message' do
+        Cat.create(name: 'Maru', weight: 12, temperament: 'curious')
+        Cat.create(name: 'Grumpy Cat', weight: 9, temperament: 'grumpy')
+
+        patch '/api/v1/cats/1', {cat: {name: nil}}
+        #binding.pry
+        response_body = JSON.parse(response.body)
+
+        expect(response.status).to eq(500)
+        expect(response_body).to eq({"name" => ["can't be blank"]})
+      end
+    end
+  end
+
 end
