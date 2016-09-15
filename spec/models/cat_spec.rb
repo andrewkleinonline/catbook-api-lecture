2c3a87bed2d907c09cb307e5aa58725916060918
Andrew Klein <andrew.klein.j@gmail.com>
2016-09-15 11:06:11 -0400

    write name and weight validation tests

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
-  it 'should be valid with a name and weight'
-  it 'should be invalid without a name'
-  it 'should be invalid without a weight'
+  it 'should be valid with a name and weight' do
+    cat = Cat.new(name: 'Maru', weight: 12)
+    expect(cat).to be_valid
+  end
+
+  it 'should be invalid without a name' do
+    bad_cat = Cat.new(weight: 10)
+    expect(bad_cat).to_not be_valid
+  end
+
+  it 'should be invalid without a weight' do
+    weightless_cat = Cat.new(name: 'Maru')
+    expect(weightless_cat).to_not be_valid
+  end
   it 'should not be fluffy be default'
   it 'should know if it is fat'
   it 'should be able to learn a new hobby'
